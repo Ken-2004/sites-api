ef2aef81ff9f4bc46e23f2f08d2e5e1b8c7ee7b6
Harsh Prajapati <hnprajapati2@myseneca.ca>
2026-02-05 20:39:34 -0500

    bug fix

--- data-service.js
+++ data-service.js
@@ -1,69 +1,104 @@
 const mongoose = require("mongoose");
 const siteSchema = require("./modules/siteSchema");
 
-let mongoDBConnectionString = process.env.MONGO_URL;
+let Site = null;
+let initPromise = null;
 
-let Site;
+function ensureInitialized() {
+  if (Site) return Promise.resolve();
 
+  if (!initPromise) {
+    const mongoDBConnectionString = process.env.MONGO_URL;
 
-module.exports.initialize = function () {
-  return new Promise(function (resolve, reject) {
+    if (!mongoDBConnectionString) {
+      return Promise.reject(
+        new Error(
+          "MONGO_URL is missing. Make sure it exists in .env (local) and Vercel Environment Variables."
+        )
+      );
+    }
+
+    initPromise = new Promise((resolve, reject) => {
       const db = mongoose.createConnection(mongoDBConnectionString);
 
-      db.on('error', err => {
-          reject(err);
+      db.on("error", (err) => {
+        reject(err);
       });
 
-      db.once('open', () => {
-          Site = db.model("sites", siteSchema);
-          // User = db.model("users", userSchema);
-          resolve();
+      db.once("open", () => {
+        Site = db.model("sites", siteSchema);
+        resolve();
       });
-  });
+    });
+  }
+
+  return initPromise;
+}
+
+module.exports.initialize = function () {
+  return ensureInitialized();
 };
 
-module.exports.addNewSite = async function (data)  {
+module.exports.addNewSite = async function (data) {
+  await ensureInitialized();
   const newSite = new Site(data);
   await newSite.save();
   return newSite;
-}
+};
 
-module.exports.getAllSites = async function (page, perPage, name, description, year, town, provinceOrTerritoryCode) {
+module.exports.getAllSites = async function (
+  page,
+  perPage,
+  name,
+  description,
+  year,
+  town,
+  provinceOrTerritoryCode
+) {
+  await ensureInitialized();
+
   let findBy = {};
+
   if (name) {
-    findBy = { "siteName": { "$regex": name, "$options": "i" } }; // contains, case insensitive
-  } 
+    findBy = { siteName: { $regex: name, $options: "i" } };
+  }
   if (description) {
-    findBy = { ...findBy, "description": { "$regex": description, "$options": "i" } }; // contains, case insensitive
-  } 
+    findBy = { ...findBy, description: { $regex: description, $options: "i" } };
+  }
   if (year) {
-    findBy = { ...findBy,  "dates.year": year };
-  } 
+    findBy = { ...findBy, "dates.year": Number(year) };
+  }
   if (town) {
-    findBy = { ...findBy, "location.town": { "$regex": town, "$options": "i" } }; // contains, case insensitive
-  } 
+    findBy = { ...findBy, "location.town": { $regex: town, $options: "i" } };
+  }
   if (provinceOrTerritoryCode) {
-    findBy = { ...findBy, "provinceOrTerritory.code": provinceOrTerritoryCode }; 
-  } ;
-
-  console.log("findBy:", findBy);
-
-  if (+page && +perPage) {
-    return Site.find(findBy).sort({ siteName: 1 }).skip((page - 1) * +perPage).limit(+perPage).exec();
+    findBy = { ...findBy, "provinceOrTerritory.code": provinceOrTerritoryCode };
   }
 
-  return Promise.reject(new Error('page and perPage query parameters must be valid numbers'));
-}
+  if (+page && +perPage) {
+    return Site.find(findBy)
+      .sort({ siteName: 1 })
+      .skip((page - 1) * +perPage)
+      .limit(+perPage)
+      .exec();
+  }
+
+  return Promise.reject(
+    new Error("page and perPage query parameters must be valid numbers")
+  );
+};
 
 module.exports.getSiteById = async function (id) {
+  await ensureInitialized();
   return Site.findById(id).exec();
-  // return Site.findOne({ _id: id }).exec(); // both work
-}
+};
 
 module.exports.updateSiteById = async function (data, id) {
+  await ensureInitialized();
   return Site.updateOne({ _id: id }, { $set: data }).exec();
-}
+};
 
 module.exports.deleteSiteById = async function (id) {
+  await ensureInitialized();
   return Site.deleteOne({ _id: id }).exec();
-}
+};
